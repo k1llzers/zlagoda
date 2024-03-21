0e9da7ae62c10ab04cf507bca96c3041d14b8ef7
Oleksandr Semytskyi <oleksandr.semytskyi@gmail.com>
2024-03-21 22:48:53 +0200

    Init commit

---
+++ src/main/java/org/naukma/zlagoda/ZlagodaApplication.java
@@ -0,0 +1,13 @@
+package org.naukma.zlagoda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZlagodaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZlagodaApplication.class, args);
+	}
+
+}
