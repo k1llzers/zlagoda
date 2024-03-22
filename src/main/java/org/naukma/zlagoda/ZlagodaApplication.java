0f258fa97650bb3c5d12470c428f8d7c25d3453e
Daria Vetrykush <dariavetrykush@gmail.com>
2024-03-22 21:27:06 +0200

    Create bean for db connection

--- src/main/java/org/naukma/zlagoda/ZlagodaApplication.java
+++ src/main/java/org/naukma/zlagoda/ZlagodaApplication.java
@@ -1,7 +1,13 @@
 package org.naukma.zlagoda;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class ZlagodaApplication {
@@ -10,4 +16,20 @@ public class ZlagodaApplication {
 		SpringApplication.run(ZlagodaApplication.class, args);
 	}
 
+	@Bean
+	public Connection getConnection(@Value("${spring.datasource.driver-class-name}") String driverClassName,
+									@Value("${spring.datasource.url}") String url,
+									@Value("${spring.datasource.username}") String user,
+									@Value("${spring.datasource.password}") String password) {
+		try {
+			Class.forName(driverClassName);
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("Can't find SQL Driver", e);
+		}
+		try {
+			return DriverManager.getConnection(url, user, password);
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
