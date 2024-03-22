9c42064771357384338398c1dea30aad4d51d4e1
Daria Vetrykush <dariavetrykush@gmail.com>
2024-03-22 22:44:52 +0200

    Fix migration for store_product

--- src/main/resources/db/migration/V6__create_store_product.sql
+++ src/main/resources/db/migration/V6__create_store_product.sql
@@ -6,5 +6,5 @@ CREATE TABLE store_product(
     products_number INT NOT NULL,
     promotional_product BOOLEAN NOT NULL,
     FOREIGN KEY (upc_prom) REFERENCES store_product(upc) ON UPDATE CASCADE ON DELETE SET NULL,
-    FOREIGN KEY (id_product) REFERENCES product(id_product) ON UPDATE CASCADE ON UPDATE NO ACTION
+    FOREIGN KEY (id_product) REFERENCES product(id_product) ON UPDATE CASCADE ON DELETE NO ACTION
 )
