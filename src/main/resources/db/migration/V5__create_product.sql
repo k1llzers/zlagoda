7884933db42f942afe165e2c87a9ad2036f67144
Oleksandr Semytskyi <oleksandr.semytskyi@gmail.com>
2024-03-22 22:26:55 +0200

    Fix category migration

--- src/main/resources/db/migration/V5__create_product.sql
+++ src/main/resources/db/migration/V5__create_product.sql
@@ -3,5 +3,5 @@ CREATE TABLE product(
     category_number INT NOT NULL,
     product_name VARCHAR(50) NOT NULL,
     characteristics VARCHAR(100) NOT NULL,
-    FOREIGN KEY (category_number) REFERENCES category(category_number)
+    FOREIGN KEY (category_number) REFERENCES category(category_number) ON UPDATE CASCADE ON DELETE NO ACTION
 )
