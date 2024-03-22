3d3968873cad70566c88ad5342bc89427055966c
Oleksandr Semytskyi <oleksandr.semytskyi@gmail.com>
2024-03-22 22:25:32 +0200

    Add product migration and fix category

---
+++ src/main/resources/db/migration/V5__create_product.sql
@@ -0,0 +1,7 @@
+CREATE TABLE product(
+    id_product SERIAL PRIMARY KEY,
+    category_number INT NOT NULL,
+    product_name VARCHAR(50) NOT NULL,
+    characteristics VARCHAR(100) NOT NULL,
+    FOREIGN KEY (category_number) REFERENCES category(category_number)
+)
