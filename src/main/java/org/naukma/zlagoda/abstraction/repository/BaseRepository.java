e430a5a8d1269f1657e94671d1b2f5c6829f3bec
Oleksandr Semytskyi <oleksandr.semytskyi@gmail.com>
2024-03-23 16:33:46 +0200

    Change to PreparedStatement

--- src/main/java/org/naukma/zlagoda/abstraction/repository/BaseRepository.java
+++ src/main/java/org/naukma/zlagoda/abstraction/repository/BaseRepository.java
@@ -3,6 +3,7 @@ package org.naukma.zlagoda.abstraction.repository;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
@@ -10,7 +11,7 @@ public abstract class BaseRepository<E, I> implements IRepository<E, I> {
     @Autowired
     protected Connection connection;
 
-    abstract protected void setMainFields(Statement statement, E entity);
+    abstract protected void setMainFields(PreparedStatement statement, E entity);
 
     abstract protected E parseSetToEntity(ResultSet set);
 }
