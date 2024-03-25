ee17e14c47e8ab7ca214524bf57966f165f15bcb
Oleksandr Semytskyi <oleksandr.semytskyi@gmail.com>
2024-03-25 23:05:27 +0200

    Refactor EmployeeMapper

--- src/main/java/org/naukma/zlagoda/employee/EmployeeMapper.java
+++ src/main/java/org/naukma/zlagoda/employee/EmployeeMapper.java
@@ -1,14 +1,10 @@
 package org.naukma.zlagoda.employee;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.NullValueCheckStrategy;
 import org.naukma.zlagoda.config.MapperConfig;
-import org.naukma.zlagoda.employee.dto.CreateUpdateEmployeeDto;
 import org.naukma.zlagoda.employee.dto.EmployeeResponseDto;
 
-@Mapper(config = MapperConfig.class, nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
+@Mapper(config = MapperConfig.class)
 public interface EmployeeMapper {
     EmployeeResponseDto toResponseDto(EmployeeEntity entity);
-
-    EmployeeEntity toEntity(CreateUpdateEmployeeDto dto);
 }
