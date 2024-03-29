5b34632727ed32b478df5387c02007873efc3765
Oleksandr Semytskyi <oleksandr.semytskyi@gmail.com>
2024-03-29 23:45:14 +0200

    Add validation dependency

---
+++ src/main/java/org/naukma/zlagoda/sale/SaleEntity.java
@@ -0,0 +1,15 @@
+package org.naukma.zlagoda.sale;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SaleEntity {
+}
