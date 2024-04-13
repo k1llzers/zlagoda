1ae76dfd6255e25c88bc3b33a6d23605ecdefa8f
Oleksandr Semytskyi <oleksandr.semytskyi@gmail.com>
2024-04-13 14:27:29 +0300

    add login picture

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -7,14 +7,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const Login = () => {
     const { setToken, setRole } = useAuth();
     const navigate = useNavigate();
-    const [email, setEmail] = useState("");
+    const [login, setLogin] = useState("");
     const [password, setPassword] = useState("");
 
     const handleLogin = async (e) => {
         e.preventDefault();
         try {
             const response = await axios.post("http://localhost:8080/api/auth/login", {
-                username: email,
+                username: login,
                 password: password
             });
             setToken(response.data.token);
@@ -26,24 +26,35 @@ const Login = () => {
     };
 
     return (
-        <form onSubmit={handleLogin}>
-            <div className="form-group">
-                <label htmlFor="exampleInputEmail1">Email address</label>
-                <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                       placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone
-                    else.</small>
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6">
+                    <img src="../login_picture.png" alt="Image" className="img-fluid"/>
+                </div>
+                <div className="col-md-6 contents">
+                    <div className="row justify-content-center">
+                        <div className="col-md-8">
+                            <div className="mb-4">
+                                <h3>Log In</h3>
+                            </div>
+                            <form action="#" method="post" onSubmit={handleLogin}>
+                                <div className="form-group first">
+                                    <label htmlFor="username">Login</label>
+                                    <input type="text" className="form-control" id="username"
+                                           value={login} onChange={(e) => setLogin(e.target.value)}/>
+                                </div>
+                                <div className="form-group last mb-4">
+                                    <label htmlFor="password">Password</label>
+                                    <input type="password" className="form-control" id="password"
+                                           value={password} onChange={(e) => setPassword(e.target.value)}/>
+                                </div>
+                                <input type="submit" value="Log In" className="btn btn-block btn-primary" />
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="form-group">
-                <label htmlFor="exampleInputPassword1">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            </div>
-            <div className="form-group form-check">
-                <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-                <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-            </div>
-            <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
+        </div>
     );
 };
 
