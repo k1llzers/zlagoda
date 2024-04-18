f18c2d4f86c64e200b3a4827b36caa11f77e19b5
Oleksandr Semytskyi <oleksandr.semytskyi@gmail.com>
2024-04-18 13:33:00 +0300

    add cors to StoreProductController

--- src/main/java/org/naukma/zlagoda/storeproduct/StoreProductController.java
+++ src/main/java/org/naukma/zlagoda/storeproduct/StoreProductController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/store-product")
+@CrossOrigin(origins = "http://localhost:3000")
 @RequiredArgsConstructor
 public class StoreProductController {
     private final StoreProductService service;
